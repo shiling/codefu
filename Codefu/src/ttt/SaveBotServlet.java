fc823c3695979fed14b8869e06069374941f78e4
Tai Shi Ling <shiling.tai@gmail.com>
2013-03-27 12:50:48 +0800

    Modified Battle.html: - Add code explanation - Implement game history playback
    Add SaveBotServlet and registered it to /ttt/save

---
+++ Codefu/src/ttt/SaveBotServlet.java
@@ -0,0 +1,44 @@
+package ttt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SaveBotServlet
+ */
+public class SaveBotServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SaveBotServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>Hello World!</title></head>");
+		out.println("<body><h1>Hello World!</h1></body></html>");
+	}
+
+}
